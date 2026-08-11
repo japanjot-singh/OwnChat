4458050ea707688779bfad22f339dfc41d200763
japanjot singh <japanjotsingh90@outlook.com>
2026-08-11 19:30:19 +0530

    Modernized the UI
    Added only one textArea for both incoming and outgoing messages.
    
    Added one TextArea for input only

--- src/ChatWindow.java
+++ src/ChatWindow.java
@@ -1,15 +1,15 @@
 import javax.swing.*;
-import javax.swing.event.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
 import java.net.*;
-import javax.swing.UIManager.*;
+
 public class ChatWindow extends JFrame implements ActionListener {
-    JPanel jps, jpr;
-    JLabel jls, jlr;
-    JTextArea jtas, jtar;
-    JButton jbs, jbr, back, cont;
+    JLabel jl1,jl2;
+    JTextArea jtcb,jtth;
+    JScrollPane jspcb,jspth;
+    JButton send,back,contacts;
+    JPanel jp;
     BufferedReader br;
     PrintWriter pw;
     Socket socket;
@@ -17,47 +17,34 @@ public class ChatWindow extends JFrame implements ActionListener {
 
     ChatWindow(String othName) {
         Container c = this.getContentPane();
-        c.setLayout(new GridLayout(1, 2));
+        c.setLayout(new BorderLayout(5,5));
         this.targetUser=othName;
-        jps = new JPanel(new BorderLayout());
-        jpr = new JPanel(new BorderLayout());
 
-        jls = new JLabel("You");
-        jls.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
-        jlr = new JLabel(othName);//add real name later
-        jlr.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
+        jl1 = new JLabel("Chat Box");
+        jtcb= new JTextArea(10,10);
+        jspcb= new JScrollPane(jtcb);
 
-        jtas = new JTextArea(10, 10);
-        jtas.setBorder(BorderFactory.createLineBorder(Color.GRAY, 5));
-        jtar = new JTextArea(10, 10);
-        jtar.setBorder(BorderFactory.createLineBorder(Color.GRAY, 5));
+        c.add(BorderLayout.NORTH,jl1);
+        c.add(BorderLayout.CENTER,jspcb);
 
-        jbs = new JButton("Send");
-        jbs.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
-        jbs.addActionListener(this);
-        jbr = new JButton("Receive");
-        jbr.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
-        jbr.addActionListener(this);
-        back = new JButton("Back");
-        back.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
-        back.addActionListener(this);
-        cont = new JButton("Contacts");
-        cont.setBorder(BorderFactory.createLineBorder(Color.GRAY, 4));
-        cont.addActionListener(this);
+        back= new JButton("Back");
+        contacts= new JButton("Contacts");
+        c.add(BorderLayout.EAST,back);
+        c.add(BorderLayout.WEST,contacts);
+
+        jp= new JPanel(new BorderLayout(3,3));
+        jtth= new JTextArea(5,10);
+        jspth= new JScrollPane(jtth);
+        jl2= new JLabel("Type Here");
+        send= new JButton("Send");
+
+        jp.add(BorderLayout.NORTH,jl2);
+        jp.add(BorderLayout.CENTER,jspth);
+        jp.add(BorderLayout.SOUTH,send);
+
+        c.add(BorderLayout.SOUTH,jp);
 
 
-        jps.add(BorderLayout.NORTH, jls);
-        jps.add(BorderLayout.CENTER, jtas);
-        jps.add(BorderLayout.SOUTH, jbs);
-        jps.add(BorderLayout.WEST, back);
-
-        jpr.add(BorderLayout.NORTH, jlr);
-        jpr.add(BorderLayout.CENTER, jtar);
-        jpr.add(BorderLayout.SOUTH, jbr);
-        jpr.add(BorderLayout.EAST, cont);
-
-        c.add(jps);
-        c.add(jpr);
 
         connectToserver();
 
@@ -68,17 +55,6 @@ public class ChatWindow extends JFrame implements ActionListener {
             Contacts_List.onWindow=false;
             Contacts_List.online_fill(Contacts_List.onWindow);
             this.dispose();
-        }
-        if (ae.getSource() == cont) {
-            Home.openClist(true);
-            this.dispose();
-        }
-        if (ae.getSource() == jbs) {
-            String msg = jtas.getText();
-            pw.println(msg);
-        }
-        if(ae.getSource() == jbr){
-
         }
 
     }
@@ -108,7 +84,7 @@ public class ChatWindow extends JFrame implements ActionListener {
                         JOptionPane.showMessageDialog(this,"User has not opened the chat window");
                     }
                     else {
-                        jtar.append(msg);
+
                     }
 
                 }
